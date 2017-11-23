b93aec554bbb94e2ca9ec52610040d50df22d186
bisco <nobu+github@glleu.net>
2017-11-23 16:16:00 +0900

    initial commit

---
+++ 2048-go.go
@@ -0,0 +1,394 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+    "strings"
+    "os"
+    "github.com/nsf/termbox-go"
+)
+
+
+type BoardState int
+const (
+    INIT_STATE BoardState = iota
+    UNDO_OK
+    REDO_OK
+)
+
+type GameBoard struct {
+    board [][]int
+    prevBoard [][]int
+    size int
+    score int
+    prevScore int
+    bstate BoardState
+}
+
+func genSquareSlice(size, inival int) [][]int {
+    s := make([][]int, size)
+    for i := 0; i < size; i++ {
+        s[i] = make([]int, size)
+        for j := 0; j < size; j++ {
+            s[i][j] = inival
+        }
+    }
+    return s
+}
+
+
+
+func GetTileColor(v int) [2]termbox.Attribute {
+    var color [2]termbox.Attribute
+    switch v {
+    case 2:
+        color[0] = termbox.Attribute(232)
+        color[1] = termbox.Attribute(23)
+    case 4:
+        color[0] = termbox.Attribute(231)
+        color[1] = termbox.Attribute(24)
+    case 8:
+        color[0] = termbox.Attribute(160)
+        color[1] = termbox.Attribute(95)
+    case 16:
+        color[0] = termbox.Attribute(17)
+        color[1] = termbox.Attribute(133)
+    case 32:
+        color[0] = termbox.Attribute(1)
+        color[1] = termbox.Attribute(76)
+    case 64:
+        color[0] = termbox.Attribute(1)
+        color[1] = termbox.Attribute(78)
+    case 128:
+        color[0] = termbox.Attribute(1)
+        color[1] = termbox.Attribute(167)
+    case 256:
+        color[0] = termbox.Attribute(1)
+        color[1] = termbox.Attribute(168)
+    case 512:
+        color[0] = termbox.Attribute(192)
+        color[1] = termbox.Attribute(54)
+    case 1024:
+        color[0] = termbox.Attribute(192)
+        color[1] = termbox.Attribute(53)
+    case 2048:
+        color[0] = termbox.Attribute(192)
+        color[1] = termbox.Attribute(89)
+    default:
+        color[0] = termbox.ColorDefault
+        color[1] = termbox.ColorDefault
+    }
+    return color
+}
+
+func NewGameBoard(size int) *GameBoard {
+    g := new(GameBoard)
+    g.size = 4
+    g.board = genSquareSlice(size, -1)
+    g.prevBoard = genSquareSlice(size, -1)
+    rand.Seed(time.Now().UTC().UnixNano())
+    x1 := rand.Intn(g.size)
+    y1 := rand.Intn(g.size)
+    x2 := rand.Intn(g.size)
+    y2 := rand.Intn(g.size)
+    if x1 != x2 || y1 != y2 {
+        g.board[x1][y1] = 2
+        g.board[x2][y2] = 2
+    }
+    g.bstate = INIT_STATE
+    g.score = 0
+    return g
+}
+
+func (g *GameBoard) PrintBoard(offset int) int {
+    drawline := func(y int) {
+        str := []rune("+" + strings.Repeat("----+", g.size))
+        for i, v := range str {
+            termbox.SetCell(i, y, v, termbox.ColorDefault, termbox.ColorDefault)
+        }
+    }
+    lineCount := 0
+    drawline(offset)
+    lineCount++ 
+    for i, row := range g.board {
+        y := i * 2 + offset
+        for j, v := range row {
+            termbox.SetCell(j*5, y+1, '|', termbox.ColorDefault, termbox.ColorDefault)
+            if v != -1 {
+                color := GetTileColor(v)
+                for k, r := range []rune(fmt.Sprintf("%4d", v)) {
+                    termbox.SetCell(j*5+1+k, y+1, r, color[0], color[1])
+                }
+            } else {
+                for k, r := range []rune(fmt.Sprintf("    ")) {
+                    termbox.SetCell(j*5+1+k, y+1, r, termbox.ColorDefault, termbox.ColorDefault)
+                }
+            }
+        }
+        termbox.SetCell((g.size+1)*4, y+1, '|', termbox.ColorDefault, termbox.ColorDefault)
+        drawline(y+2)
+        lineCount += 2
+    }
+    return lineCount
+}
+
+func (g *GameBoard) PrintScore(offset int) int {
+    str := fmt.Sprintf("Score: %d", g.score)
+    for i, r := range []rune(str) {
+        termbox.SetCell(i, offset, r, termbox.ColorDefault, termbox.ColorDefault)
+    }
+    return 1
+}
+
+func PrintUsage(offset int) int {
+    usage := "ESC: Exit the game  PgDn: Undo  PgUp: Redo"
+    for i, r := range []rune(fmt.Sprintf(usage)) {
+        termbox.SetCell(i, offset, r, termbox.ColorDefault, termbox.ColorDefault)
+    }
+    return 1
+}
+
+func (g *GameBoard) Draw() {
+    termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+    offset := 0
+    offset += g.PrintScore(offset)
+    offset += g.PrintBoard(offset)
+    offset += PrintUsage(offset)
+
+    termbox.Flush()
+}
+
+func (g *GameBoard) KeepPrevBoard() {
+    g.bstate = REDO_OK
+    for i, row := range g.board {
+        for j, v := range row {
+            g.prevBoard[i][j] = v
+        }
+    }
+    g.prevScore = g.score
+}
+
+func (g *GameBoard) swapBoard() {
+    for i, row := range g.board {
+        for j, _ := range row {
+            g.prevBoard[i][j], g.board[i][j] = g.board[i][j], g.prevBoard[i][j]
+        }
+    }
+}
+
+func (g *GameBoard) mirrorLR() {
+    for i := 0; i < g.size; i++ {
+        for j := 0; j < g.size/2; j++ {
+            g.board[i][j], g.board[i][g.size-1-j] = g.board[i][g.size-1-j], g.board[i][j]
+        }
+    }
+}
+
+func (g *GameBoard) rrot90() {
+    board := genSquareSlice(g.size, -1)
+    for i := 0; i < g.size; i++ {
+        for j := 0; j < g.size; j++ {
+            board[i][j] = g.board[g.size - 1 - j][i]
+        }
+    }
+    g.board = board
+}
+
+func (g *GameBoard) lrot90() {
+    board := genSquareSlice(g.size, -1)
+    for i := 0; i < g.size; i++ {
+        for j := 0; j < g.size; j++ {
+            board[i][j] = g.board[j][g.size - 1 - i]
+        }
+    }
+    g.board = board
+}
+
+func (g *GameBoard) Undo() {
+    if g.bstate == UNDO_OK {
+        g.swapBoard()
+        g.bstate = REDO_OK
+    }
+}
+
+func (g *GameBoard) Redo() {
+    if g.bstate == REDO_OK {
+        g.swapBoard()
+        g.bstate = UNDO_OK
+    }
+}
+
+func (g *GameBoard) mergeTile(dsty, dstx, srcy, srcx int) {
+    g.board[srcy][srcx] = -1
+    g.board[dsty][dstx] *= 2
+    g.score += g.board[dsty][dstx]
+}
+
+func (g *GameBoard) canSlideRight(y, x, limit int) bool {
+    if x == g.size - 1 {
+        return false
+    }
+    if g.board[y][x] == g.board[y][x+1] {
+        return true
+    }
+    for i := x + 1; i < limit; i++ {
+        if g.board[y][i] == -1 {
+            return true
+        }
+    }
+    return false
+}
+
+func (g *GameBoard) _slideRight(y, x, limit int) int {
+    for i := x + 1; i < limit; i++ {
+        if g.board[y][i] == -1 {
+            continue
+        } else if g.board[y][i] == g.board[y][x] {
+            g.mergeTile(y, i, y, x)
+            return i
+        } else {
+            g.board[y][i-1] = g.board[y][x]
+            if !(i == 1 && x == 0) {
+                g.board[y][x] = -1
+            }
+            return i - 1
+        }
+    }
+    g.board[y][limit - 1] = g.board[y][x]
+    g.board[y][x] = -1
+    return limit
+}
+
+func (g *GameBoard) SlideRight() bool {
+    moved := false
+    limit := g.size
+    for i, row := range g.board {
+        for j := g.size - 1; j >= 0; j-- {
+            if row[j] == -1 {
+                continue
+            }
+            if g.canSlideRight(i, j, limit) {
+                limit = g._slideRight(i, j, limit)
+                moved = true
+            }
+        }
+        limit = g.size
+    }
+    return moved
+}
+
+func (g *GameBoard) PopNewTile(){
+    for {
+        x := rand.Intn(g.size)
+        y := rand.Intn(g.size)
+        if g.board[x][y] == -1 {
+            if rand.Intn(100) < 90 {
+                g.board[x][y] = 2
+            } else {
+                g.board[x][y] = 4
+            }
+            break
+        }
+    }
+}
+
+func (g *GameBoard) SlideUp() bool {
+    g.rrot90()
+    ret := g.SlideRight()
+    g.lrot90()
+    return ret
+}
+
+func (g *GameBoard) SlideDown() bool {
+    g.lrot90()
+    ret := g.SlideRight()
+    g.rrot90()
+    return ret
+}
+
+func (g *GameBoard) SlideLeft() bool {
+    g.mirrorLR()
+    ret := g.SlideRight()
+    g.mirrorLR()
+    return ret
+}
+
+func handleKeyEvent(ev termbox.Event) bool {
+    switch ev.Type {
+    case termbox.EventKey:
+        switch ev.Key {
+        case termbox.KeyEsc:
+            return false
+        case termbox.KeyArrowLeft:
+            g.KeepPrevBoard()
+            if g.SlideLeft() {
+                g.PopNewTile()
+            }
+            g.Draw()
+        case termbox.KeyArrowRight:
+            g.KeepPrevBoard()
+            if g.SlideRight() {
+                g.PopNewTile()
+            }
+            g.Draw()
+        case termbox.KeyArrowUp:
+            g.KeepPrevBoard()
+            if g.SlideUp() {
+                g.PopNewTile()
+            }
+            g.Draw()
+        case termbox.KeyArrowDown:
+            g.KeepPrevBoard()
+            if g.SlideDown() {
+                g.PopNewTile()
+            }
+            g.Draw()
+        case termbox.KeyPgup:
+            g.Undo()
+            g.Draw()
+        case termbox.KeyPgdn:
+            g.Redo()
+            g.Draw()
+        default:
+            g.Draw()
+        }
+    default:
+        g.Draw()
+    }
+    return true
+}
+
+var g *GameBoard
+
+func main() {
+    err := termbox.Init()
+    if err != nil {
+        fmt.Fprintln(os.Stderr, err)
+        os.Exit(1)
+    }
+    defer termbox.Close()
+    termbox.SetOutputMode(termbox.Output256)
+
+    size := 4
+    g = NewGameBoard(size)
+    evc := make(chan termbox.Event)
+    go func() {
+        for {
+            evc <-termbox.PollEvent()
+        }
+    }()
+
+    g.Draw()
+    for {
+        select {
+        case ev := <-evc:
+            if !handleKeyEvent(ev) {
+                return
+            }
+            g.Draw()
+        case <-time.After(1*time.Second):
+            g.Draw()
+        }
+    }
+}
---
+++ 2048-go_test.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+    "fmt"
+    "testing"
+)
+
+func TestMakingBoard(t *testing.T) {
+    size := 4
+    g := NewGameBoard(size)
+    if g.size != size {
+        t.Error("GameBoard size is should be 4")
+    }
+    for _, row := range g.board {
+        for _, v := range row {
+            if !(v == 2 || v == -1) {
+                t.Error("Initial value should be 2 or -1(as an invalid number)")
+            }
+        }
+    }
+}
+
+type Direction int
+
+const (
+    UP Direction = iota
+    DOWN
+    LEFT
+    RIGHT
+)
+
+func TestSlideTile(t *testing.T) {
+    size := 4
+    g := NewGameBoard(size)
+
+    type testcase struct {
+        x1, y1 int
+        x2, y2 int
+        move bool
+        d Direction
+        ansx1, ansy1 int
+        ans1 int
+        ansx2, ansy2 int
+        ans2 int
+    }
+
+    clearBoard := func() {
+        for i, row := range g.board {
+            for j, _ := range row {
+                g.board[i][j] = -1
+            }
+        }
+    }
+
+    test := func(tc testcase) bool {
+        clearBoard()
+        g.board[tc.y1][tc.x1] = 2
+        g.board[tc.y2][tc.x2] = 2
+        var result bool
+        switch tc.d {
+        case UP:
+            result = g.SlideUp()
+        case DOWN:
+            result = g.SlideDown()
+        case LEFT:
+            result = g.SlideLeft()
+        case RIGHT:
+            result = g.SlideRight()
+
+        }
+        if result != tc.move {
+            t.Error("Slidable check")
+            return false
+        }
+        if tc.ans1 != -1 && g.board[tc.ansy1][tc.ansx1] != tc.ans1 {
+            t.Error("ans1 <->", tc.ans1)
+            return false
+        }
+        if tc.ans2 != -1 && g.board[tc.ansy2][tc.ansx2] != tc.ans2 {
+            t.Error("ans2 <->", g.board[tc.ansy2][tc.ansx2], tc.ans2)
+            return false
+        }
+        for i, row := range g.board {
+            for j, v := range row {
+                if i == tc.ansy1 && j == tc.ansx1 {
+                    continue
+                }
+                if i == tc.ansy2 && j == tc.ansx2 {
+                    continue
+                }
+                if v != -1 {
+                    t.Error("Initial value should -1 (as an invalid number) => (x, y) = (",j ,",", i, ",",g.board[i][j],")")
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    testcases := []testcase{
+        testcase{0, 0, 1, 0, true,  LEFT,  0, 0, 4, 0, 0, -1}, 
+        testcase{0, 0, 1, 1, true,  LEFT,  0, 0, 2, 0, 1,  2}, 
+        testcase{0, 0, 0, 1, false, LEFT,  0, 0, 2, 0, 1,  2}, 
+        testcase{0, 0, 3, 3, true,  LEFT,  0, 0, 2, 0, 3,  2}, 
+        testcase{1, 2, 2, 3, true,  LEFT,  0, 2, 2, 0, 3,  2}, 
+        testcase{2, 2, 3, 2, true,  LEFT,  0, 2, 4, 0, 0, -1}, 
+
+        testcase{0, 0, 1, 0, true,  RIGHT, 3, 0, 4, 0, 0, -1}, 
+        testcase{0, 0, 1, 1, true,  RIGHT, 3, 0, 2, 3, 1,  2}, 
+        testcase{3, 0, 3, 1, false, RIGHT, 3, 0, 2, 3, 1,  2}, 
+        testcase{0, 0, 3, 3, true,  RIGHT, 3, 0, 2, 3, 3,  2}, 
+        testcase{1, 2, 2, 3, true,  RIGHT, 3, 2, 2, 3, 3,  2}, 
+        testcase{2, 2, 3, 2, true,  RIGHT, 3, 2, 4, 0, 0, -1}, 
+
+        testcase{0, 0, 1, 0, false,    UP, 0, 0, 2, 1, 0,  2}, 
+        testcase{0, 0, 1, 1, true,     UP, 0, 0, 2, 1, 0,  2}, 
+        testcase{3, 0, 3, 1, true,     UP, 3, 0, 4, 3, 1, -1}, 
+        testcase{0, 0, 3, 3, true,     UP, 0, 0, 2, 3, 0,  2}, 
+        testcase{1, 2, 2, 3, true,     UP, 1, 0, 2, 2, 0,  2}, 
+        testcase{2, 2, 3, 2, true,     UP, 2, 0, 2, 3, 0,  2}, 
+
+        testcase{0, 0, 1, 0, true,   DOWN, 0, 3, 2, 1, 3,  2}, 
+        testcase{0, 0, 1, 1, true,   DOWN, 0, 3, 2, 1, 3,  2}, 
+        testcase{3, 0, 3, 1, true,   DOWN, 3, 3, 4, 3, 1, -1}, 
+        testcase{0, 0, 3, 3, true,   DOWN, 0, 3, 2, 3, 3,  2}, 
+        testcase{1, 2, 2, 3, true,   DOWN, 1, 3, 2, 2, 3,  2}, 
+        testcase{2, 3, 3, 3, false,  DOWN, 2, 3, 2, 3, 3,  2}, 
+    }
+
+    for _, v := range testcases {
+        if test(v) {
+            fmt.Printf("%v is passed\n", v)
+        } else {
+            fmt.Printf("%v is failed\n", v)
+        }
+    }
+}
+
+func TestSlideTileRight(t *testing.T) {
+    size := 4
+    g := NewGameBoard(size)
+    clearBoard := func() {
+        for i, row := range g.board {
+            for j, _ := range row {
+                g.board[i][j] = -1
+            }
+        }
+    }
+    clearBoard()
+    start := [4][4]int{{ 2,  2, -1, -1},
+                       {-1, -1,  4, -1},
+                       {-1,  2, -1,  2},
+                       { 4, -1,  2, -1},
+                      }
+    ans := [4][4]int{{-1, -1, -1,  4},
+                     {-1, -1, -1,  4},
+                     {-1, -1, -1,  4},
+                     {-1, -1, 4,  2},
+                    }
+    for i, row := range start {
+        for j, v := range row {
+            g.board[i][j] = v
+        }
+    }
+    if !g.SlideRight() {
+        t.Error("Slidable Check")
+    }
+    for i, row := range ans {
+        for j, v := range row {
+            if g.board[i][j] != v {
+                t.Error("merge miss")
+                fmt.Printf("(%d, %d) has %d, but expected %d\n", i, j, g.board[i][j], v)
+            }
+        }
+    }
+}
